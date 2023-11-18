308fa30b81c62bc5502e20f8039af85253677f02
ManuTorrado <manuel.torrado@gmail.com>
2023-11-18 00:53:59 -0300

    First commit

---
+++ src/main/java/com/example/concesionariaapi/ConcesionariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.concesionariaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionariaApiApplication.class, args);
+	}
+
+}
